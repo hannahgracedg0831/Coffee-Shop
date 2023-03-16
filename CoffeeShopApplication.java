aa8601a4f4ce32966a3141cd13692745f312ab22
Hannah Grace <gutierrezhannah70@gmail.com>
2023-03-16 17:40:00 +0800

    update class

--- CoffeeShopApplication.java
+++ CoffeeShopApplication.java
@@ -1,11 +1,24 @@
 import javax.swing.JFrame;
 
 public class CoffeeShopApplication {
+    private JFrame frame;
 
-    public Object frame;
-
-    public JFrame getFrame() {
-        return null;
+    public CoffeeShopApplication() {
+        initialize();
     }
 
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        frame.setVisible(true); // set JFrame visible
+    }
+
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    // other methods, etc.
 }
